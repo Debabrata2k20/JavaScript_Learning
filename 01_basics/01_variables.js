8de98bdda76fc3b592fd8525d7999f0b7e418e80
Debabrata Sahu <debabratasahu10.p@gmail.com>
2024-11-25 18:47:10 +0530

    first commit-upload JS variable file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId= 123456
+let accountEmail="debabrata10.p@gmail.com"
+var accountPassword= "1223"
+accountCity= "Gunupur"
+
+console.log(accountId)
+
+
+accountEmail="debab@gmail.com"
+accountPassword="1222234"
+accountCity="Hyderabad"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
